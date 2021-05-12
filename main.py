d5b95c7c9f5a7afb3c622ccdb33a26662c0841cb
Nick Watson <>
2021-05-12 05:06:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+        newList = []
+        newNum = 0
+
+        for num in nums:
+            if newList == []:
+                newList.append(num)
+                newNum += num
+            else:
+                newNum += num
+                newList.append(newNum)
+        return newList
+        
+        
+'''
+
+U:
+
+[1, 2, 3, 4, 5]
+output = [1, 3, 6, 10, 15]
+
+[2, 4, 6, 8]
+output = [2, 6, 12, 20]
+
+[1, 3, -4, -6]
+output = [1, 4, 0, -6]
+
+P:
+
+iterate through array, add each index to previous answer
+
+'''
